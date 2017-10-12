d6a8215642a103806c5cae530f65b8668e207c51
GruneEnterTaste <daniel.waldvogel@gmx.ch>
2017-10-12 20:03:57 +0200

    Version:                           0.008 Autor:                              Daniel Waldvogel Datum:                            12.10.2017 Zeit:                                 19:55 ---------------------------------------------------------------------------------- Änderungen: Code Aufräumen Branches zusammen führen

--- HSR-Dechiffre-Entcoder/app/src/main/gen/ch/derbeton/hsr_dechiffre_entcoder/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package ch.derbeton.hsr_dechiffre_entcoder;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {
-}
