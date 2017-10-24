fc044a59ad8e012340ea5418e91d8c76ce9610f1
GruneEnterTaste <daniel.waldvogel@gmx.ch>
2017-10-24 20:21:20 +0200

    Version:                           0.015 Autor:                              Daniel Waldvogel Datum:                            24.10.2017 Zeit:                                 20:21 ---------------------------------------------------------------------------------- Änderungen: N Paar Sachen Bei der Memory Activity eingefügt. Hat noch nen kleinen Fehler ^^ (Bei in Memory Activity unten das auskommentierte)

--- MetallDetectorApp/app/src/main/java/ch/derbeton/metalldetectorapp/Memory.java
+++ MetallDetectorApp/app/src/main/java/ch/derbeton/metalldetectorapp/Memory.java
@@ -3,14 +3,23 @@ package ch.derbeton.metalldetectorapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.zxing.client.android.Intents;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.journeyapps.barcodescanner.CaptureActivity;
 
 public class Memory extends AppCompatActivity {
 
 
     // for the Logbuch app
     private static final int SCAN_QR_CODE_REQUEST_CODE = 0;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     //---------------------------------------------------------------------------------------------------------------------
     // Menu Start
@@ -91,6 +100,47 @@ public class Memory extends AppCompatActivity {
 //---------------------------------------------------------------------------------------------------------------------
 
 
+
+
+
+// Foto aufnehmen / Code auslesen
+    public void takeQrCodePicture() {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setCaptureActivity(MyCaptureActivity.class);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+        integrator.setOrientationLocked(false);
+        integrator.addExtra(Intents.Scan.BARCODE_IMAGE_ENABLED, true);
+        integrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == IntentIntegrator.REQUEST_CODE
+                && resultCode == RESULT_OK) {
+
+            Bundle extras = data.getExtras();
+            String path = extras.getString(
+                    Intents.Scan.RESULT_BARCODE_IMAGE_PATH);
+
+            // Ein Bitmap zur Darstellung erhalten wir so:
+            // Bitmap bmp = BitmapFactory.decodeFile(path)
+
+            String code = extras.getString(
+                    Intents.Scan.RESULT);
+        }
+    }
+
+    public class MyCaptureActivity extends CaptureActivity { }
+
+    // Recyclerview
+    RecyclerView rv = (RecyclerView)findViewById(R.id.recyclerView);
+    GridLayoutManager gridLayoutManager = new GridLayoutManager(this /* the activity */, 2);
+
+    // funkt noch nicht
+    // rv.LayoutManager(gridLayoutManager);
+
+
+    // Ende
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
